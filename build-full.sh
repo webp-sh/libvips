af9e5e9d23581473168faea9b4fd795326a075e4
n0vad3v <n0vad3v@riseup.net>
2024-02-11 12:28:56 +0800

    Upgrade to Go 1.22

--- build-full.sh
+++ build-full.sh
@@ -3,10 +3,10 @@ ARCH=$(uname -m)
 
 case $ARCH in
     "x86_64")
-        FILE="go1.21.0.linux-amd64.tar.gz"
+        FILE="go1.22.0.linux-amd64.tar.gz"
         ;;
     "aarch64")
-        FILE="go1.21.0.linux-arm64.tar.gz"
+        FILE="go1.22.0.linux-arm64.tar.gz"
         ;;
     *)
         echo "Unsupported architecture: $ARCH"
